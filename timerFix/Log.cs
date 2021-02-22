8d541447a9e74f50ffaba5b726379cbe54f3a975
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-02-22 15:52:39 +0000

    first commit

---
+++ timerFix/Log.cs
@@ -0,0 +1,22 @@
+﻿using BWModLoader;
+
+namespace timerFix
+{
+    internal static class Log
+    {
+        static readonly public ModLogger logger = new ModLogger("[TimerFix]", ModLoader.LogPath + "\\Alternion.txt");
+    }
+    public class Logger
+    {
+        /// <summary>
+        /// Always logs, no matter the logging level.
+        /// </summary>
+        /// <param name="message">Message to Log</param>
+        public static void log(string message)
+        {
+            //Just easier to type than Log.logger.Log
+            //Will always log, so only use in try{} catch(Exception e) {} when absolutely needed
+            Log.logger.Log(message);
+        }
+    }
+}
---
+++ timerFix/mainmod.cs
@@ -0,0 +1,133 @@
+﻿using Harmony;
+using BWModLoader;
+using UnityEngine;
+using System.IO;
+
+namespace timerFix
+{
+    [Mod]
+    public class mainmod : MonoBehaviour
+    {
+
+        public static mainmod Instance;
+        static Vector3 defaultPos;
+        static Vector3 offset = new Vector3(0, 0, 0);
+        static string configFile = "timerFix.cfg";
+        static bool changeRuntime = false;
+        static string updateKey = "*";
+
+        void Awake()
+        {
+            if (!Instance)
+            {
+                Instance = this;
+            }
+            else
+            {
+                DestroyImmediate(this);
+            }
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(updateKey) && changeRuntime)
+            {
+                loadFromConfig();
+                if (UI.Instance.matchHUD != null)
+                {
+                    applyOffset();
+                }
+            }
+        }
+
+        void loadFromConfig()
+        {
+            fileExists();
+            string[] array = File.ReadAllLines(configFile);
+            char splitCharacter = '=';
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Contains("="))
+                {
+                    string[] splitArr = array[i].Split(splitCharacter);
+                    if (splitArr.Length >= 2)
+                    {
+                        switch (splitArr[0])
+                        {
+                            case "Change_at_runtime":
+                                bool.TryParse(splitArr[1], out changeRuntime);
+                                break;
+                            case "Update_key":
+                                updateKey = splitArr[1];
+                                break;
+                            case "x":
+                                float.TryParse(splitArr[1], out offset.x);
+                                break;
+                            case "y":
+                                float.TryParse(splitArr[1], out offset.y);
+                                break;
+                            case "z":
+                                float.TryParse(splitArr[1], out offset.z);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+            }
+
+            Logger.log("Loaded offset: " + offset.ToString());
+        }
+
+        void fileExists()
+        {
+            if (!File.Exists(configFile))
+            {
+                StreamWriter streamWriter = new StreamWriter(configFile);
+                streamWriter.WriteLine("Update_key=*");
+                streamWriter.WriteLine("Change_at_runtime=true");
+                streamWriter.WriteLine("x=0");
+                streamWriter.WriteLine("y=0");
+                streamWriter.WriteLine("z=0");
+                streamWriter.Close();
+            }
+        }
+
+        void Start()
+        {
+            //Setup harmony patching
+            HarmonyInstance harmony = HarmonyInstance.Create("com.github.archie");
+            harmony.PatchAll();
+
+            loadFromConfig();
+        }
+
+        static void applyOffset()
+        {
+            UI.Instance.matchHUD.íðæåñóççíìè.transform.localPosition = defaultPos;
+            Logger.log("Set to Default: " + defaultPos.ToString());
+            UI.Instance.matchHUD.íðæåñóççíìè.transform.localPosition += offset;
+            Logger.log("Set offset: " + offset.ToString());
+            Logger.log("Set to: " + UI.Instance.matchHUD.íðæåñóççíìè.transform.localPosition.ToString());
+        }
+
+        [HarmonyPatch(typeof(MatchHUD), "Start")]
+        static class getHudPatch
+        {
+            static void Postfix(MatchHUD __instance)
+            {
+                defaultPos = UI.Instance.matchHUD.íðæåñóççíìè.transform.localPosition;
+                Logger.log("Set Default: " + defaultPos.ToString());
+            }
+        }
+
+        [HarmonyPatch(typeof(MatchHUD), "OnEnable")]
+        static class applyOffsetPatch
+        {
+            static void Postfix()
+            {
+                applyOffset();
+            }
+        }
+    }
+}
